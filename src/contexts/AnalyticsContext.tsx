4b09c7ab6d212a3f8ef92886a6dc1facddabd6cc
Jackson Miller <jackson@jackson.codes>
2025-02-06 20:25:04 -0500

    lint

--- src/contexts/AnalyticsContext.tsx
+++ src/contexts/AnalyticsContext.tsx
@@ -6,16 +6,18 @@ import React, {
   useContext,
   useEffect,
 } from "react";
-import { env } from "@/env";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
-import mixpanel from "mixpanel-browser";
+import mixpanel, { type Config } from "mixpanel-browser";
 import LogRocket from "logrocket";
 import * as amplitude from "@amplitude/analytics-browser";
 import { useUser } from "@clerk/nextjs";
 
 interface AnalyticsContextType {
-  captureEvent: (eventName: string, properties?: Record<string, any>) => void;
+  captureEvent: (
+    eventName: string,
+    properties?: Record<string, unknown>,
+  ) => void;
   identifyUser: (userId: string) => void;
 }
 
@@ -35,13 +37,13 @@ export function AnalyticsProvider({ children }: { children: React.ReactNode }) {
       debug: true,
       // types don't match the docs for this one, so we need to cast it to any
       autocapture: true,
-    } as any);
+    } as unknown as Config);
     LogRocket.init("rmdvl6/last-horizon");
     amplitude.init("774e3c5338de61ff40858286506bd47d", { autocapture: true });
   }, []);
 
   const captureEvent = useCallback(
-    (eventName: string, properties?: Record<string, any>) => {
+    (eventName: string, properties?: Record<string, unknown>) => {
       if (typeof window === "undefined") return;
 
       posthog.capture(eventName, properties);
@@ -49,18 +51,15 @@ export function AnalyticsProvider({ children }: { children: React.ReactNode }) {
       LogRocket.log(eventName, properties);
       amplitude.track(eventName, properties);
     },
-    [posthog],
+    [],
   );
 
-  const identifyUser = useCallback(
-    (userId: string) => {
-      posthog.identify(userId);
-      mixpanel.identify(userId);
-      LogRocket.identify(userId);
-      amplitude.setUserId(userId);
-    },
-    [posthog],
-  );
+  const identifyUser = useCallback((userId: string) => {
+    posthog.identify(userId);
+    mixpanel.identify(userId);
+    LogRocket.identify(userId);
+    amplitude.setUserId(userId);
+  }, []);
 
   const unidentifyUser = useCallback(() => {
     posthog.reset();
