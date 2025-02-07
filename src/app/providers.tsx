ced732b408b622cdda5fad0d9e1dc0f3b8774ffd
Jackson Miller <jackson@jackson.codes>
2025-02-06 19:18:18 -0500

    add logrocket

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { env } from "@/env";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import mixpanel from "mixpanel-browser";
+import LogRocket from "logrocket";
 
 if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
@@ -16,6 +17,7 @@ if (typeof window !== "undefined") {
     // types don't match the docs for this one, so we need to cast it to any
     autocapture: true,
   } as any);
+  LogRocket.init("rmdvl6/last-horizon");
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
